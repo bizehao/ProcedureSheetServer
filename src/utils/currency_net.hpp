3d16d1bce35b8790e79fda0b1f4fa6c8cc799ce6
暮色游侠 <2422148686@qq.com>
2019-10-28 13:15:51 +0800

    11

--- src/utils/currency_net.hpp
+++ src/utils/currency_net.hpp
@@ -8,22 +8,58 @@
 #include "cinatra.hpp"
 #include <functional>
 #include "reflex.hpp"
+#include <array>
+
+template<size_t ...N, typename Fun, typename Ob, typename Tup>
+static void invokeOfTuple(const std::index_sequence<N...>&, Fun& fun, Ob ob, Tup& tup) {
+    std::invoke(std::forward<Fun>(fun), std::forward<Ob>(ob), std::get<N>(tup)...);
+}
+
+//元祖大小 数组大小 下标值 4  2  0
+template<size_t CN, size_t AN, size_t N, typename Tup>
+static auto getNextValue(std::array<std::string, AN>& array, Tup& tup) {
+    if constexpr (N + AN < CN - 1) {
+        getNextValue<CN, AN, N + 1>(array, tup);
+    }
+    array[N] = std::get<CN - AN + N>(tup);
+}
 
 template<size_t size, typename O, typename ...T>
-static void joinNet(O &o, cinatra::http_server &server, const std::tuple<T...> &vTuple) {
+static void joinNet(O& o, cinatra::http_server& server, const std::tuple<T...>& vTuple) {
     if constexpr (size > 0) {
         joinNet<size - 1>(o, server, vTuple);
     }
     auto to = std::get<size>(vTuple);
+    constexpr size_t ss = std::tuple_size_v<decltype(to)>; //参数长度
     std::cout << std::get<0>(to) << std::endl;
     server.set_http_handler<cinatra::GET>(std::get<0>(to),
-                                          [&o, to](cinatra::request &req, cinatra::response &res) {
-                                              std::invoke(std::get<1>(to), std::forward<O>(o), req, res);
+                                          [&o, to, ss](cinatra::request& req, cinatra::response& res) {
+                                              auto xx = std::get<1>(to); //函数值
+                                              constexpr size_t args_size = std::tuple_size_v<member_args_t<decltype(xx)>>;
+                                              std::cout << args_size << std::endl;
+
+                                              std::cout << "参数长度" << ss << std::endl;
+                                              std::array<std::string, ss - 2> array; //参数数组
+                                              getNextValue<ss, array.size(), 0>(array, to);
+
+                                              //int a = req.get_query_value<int>(array[0]);
+                                              //int b = req.get_query_value<int>(array[1]);
+                                              auto tup = a2t<hangArray_lambda, member_args_t<decltype(xx)>>(array, req);
+
+                                              /*auto tup = a2t(array, [&req](auto index,const std::string& v) {
+                                                  std::cout << v << std::endl;
+                                                  return req.get_query_value<std::tuple_element_t<index, member_args_t<decltype( xx )>>>(v);
+                                              });*/
+                                              invokeOfTuple(std::make_index_sequence<2>(),
+                                                            std::get<1>(to),
+                                                            std::forward<O>(o),
+                                                            tup
+                                              );
                                           });
 }
 
 template<typename O, typename ...T>
-void execNet(O &o, cinatra::http_server &server, T ...t) {
+void execNet(O& o, cinatra::http_server& server, T ...t) {
     joinNet<sizeof...(t) - 1>(o, server, std::make_tuple(t...));
 }
 
